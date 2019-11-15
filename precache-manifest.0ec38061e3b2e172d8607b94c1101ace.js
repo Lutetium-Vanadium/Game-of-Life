1d1f63f260b56a16f25365b2c21fd951a7e11781
Lutetium-Vanadium <luv.s7000@gmail.com>
2019-11-15 16:25:51 +0530

    Updates

---
+++ precache-manifest.0ec38061e3b2e172d8607b94c1101ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9fecf174fd45e170d09a46f3ceaa7",
+    "url": "/Game-of-Life/index.html"
+  },
+  {
+    "revision": "5a225ee7dfa2e2e4c786",
+    "url": "/Game-of-Life/static/css/main.c8e818df.chunk.css"
+  },
+  {
+    "revision": "f0ce06d8e9c692002b6e",
+    "url": "/Game-of-Life/static/js/2.d81e214b.chunk.js"
+  },
+  {
+    "revision": "5a225ee7dfa2e2e4c786",
+    "url": "/Game-of-Life/static/js/main.6bb5106c.chunk.js"
+  },
+  {
+    "revision": "c15d5444f2422f1015de",
+    "url": "/Game-of-Life/static/js/runtime-main.3811f2cb.js"
+  }
+]);
